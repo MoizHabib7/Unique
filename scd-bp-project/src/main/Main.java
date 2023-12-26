db485e26171478e194f6b124bcd1ed89eb2730ff
sanaullah khatri <sanaullah.zulfiqar020@gmail.com>
2023-12-26 22:53:21 +0500

    add to cart web services

--- scd-bp-project/src/main/Main.java
+++ scd-bp-project/src/main/Main.java
@@ -1,10 +1,19 @@
 package main;
-
+import java.sql.*;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con =
+			DriverManager.getConnection("jdbc:mysql://localhost:3306/project","root","");
+			Statement stmt=con.createStatement();
+			stmt.executeUpdate("INSERT INTO `cart`(`Product_id`, `Product_name`, `Price`, `discount`, `quantity`, `user_id`) VALUES (3,'cloths',100,10,70,1);");
 
+			con.close();
+			} catch(Exception e)
+			{
+			System.out.println(e);
+			}
 	}
-
 }
