882b0212f05b6afde30fd86ad36d88de214050c2
sanaullah khatri <sanaullah.zulfiqar020@gmail.com>
2024-01-02 15:20:08 +0500

    change

--- scd-bp-project/src/main/buy_product.java
+++ scd-bp-project/src/main/buy_product.java
@@ -3,18 +3,20 @@ import java.sql.*;
 
 public class buy_product {
 		public void add_to_cart(String add,String date,int pay,String user) {
-
 			try{
 				Class.forName("com.mysql.jdbc.Driver");
 				Connection con =
 				DriverManager.getConnection("jdbc:mysql://localhost:3306/project","root","");
-				Statement stmt=con.createStatement();
-				stmt.executeUpdate("INSERT INTO order(`address`, `date_of_delivery`, `payment mode`, `user_name`) VALUES ('"+add+"',"+date+","+pay+","+user+");");
-
+				PreparedStatement stmt=con.prepareStatement("INSERT INTO `order`(`user_name`, `address`, `date_of_delivery`, `payment mode`) VALUES (?,?,?,?);");
+				stmt.setString(1, user);
+				stmt.setString(2, add);
+				stmt.setString(3, date);
+				stmt.setInt(4, pay);				
+				stmt.executeUpdate();
 				con.close();
 				} catch(Exception e)
 				{
-				
+					e.printStackTrace();
 				}
 		}
 }
